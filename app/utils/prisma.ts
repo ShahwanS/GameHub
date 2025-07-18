f58b4fd7ddd4de3d35a7ad64e380dc06b601c9cd
seif shahwan <saif.shahwan@icloud.com>
2025-07-18 06:49:45 +0200

    Refactor Prisma integration and update package configuration
    - Changed project name in package.json to "@repo/gamesprout".
    - Removed Prisma schema and generated client files, streamlining database integration.
    - Updated build script in package.json to remove unnecessary Prisma generation step.
    - Adjusted TypeScript version in package.json for compatibility.
    - Refactored Prisma client usage in app/utils/prisma.ts to utilize a singleton pattern.

--- app/utils/prisma.ts
+++ app/utils/prisma.ts
@@ -1,15 +1,4 @@
-import { PrismaClient } from '@prisma/client'
+import { prisma } from '@repo/database';
 
-const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
-
-declare global {
-  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
-}
-
-export const prisma = globalThis.prisma ?? prismaClientSingleton()
-
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
-
-export default prisma
+export { prisma };
+export default prisma;
